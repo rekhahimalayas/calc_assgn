9ecaf8fdafba73b869fc3a8bfbba859ffdd8b274
Rekha S <rekhas@Rekhas-MacBook-Air.local>
2023-09-22 17:03:59 +0530

    changes in calc_app.java

--- calc_app/calc/calc_app.java
+++ calc_app/calc/calc_app.java
@@ -3,12 +3,13 @@ package calc_app.calc;
 
 import calc_app.calc.factories.adv_calc_factory;
 import calc_app.calc.factories.icalc_factory;
-import factories.*;
+
 
 public class calc_app {
     public static void main(String[] atr) {
         icalc_factory fact = new adv_calc_factory();
-        double res = fact.create_calc().sqrt(78);
+        icalculator1 calc = new advanced_calculator();
+        double res = calc.add(78, 34);
         System.out.println("res: " + res);
     }
 }
